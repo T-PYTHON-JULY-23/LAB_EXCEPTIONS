fd01f2be19b64ecc74ab8b64b854dafb3d38ca1d
sarahnaghi <sara.naghi@gmail.com>
2023-07-31 22:47:05 +0300

    exceptions lab done

---
+++ LAB_EXCEPTIONS.py
@@ -0,0 +1,14 @@
+
+
+def additoin(x, y):
+    x = 10
+    y = 20
+try:
+    print("Addition:", x + b) 
+    print("the operation is successful")
+
+except NameError:
+        print("not defined variable! make sure you define your variables")
+
+additoin(10, 20)
+
---
+++ bonus.py
@@ -0,0 +1,40 @@
+
+def fahrenheit_to_celsius(fahrenheit):
+    celsius = (fahrenheit - 32) * 5/9
+    return celsius
+
+def celsius_to_fahrenheit(celsius):
+    fahrenheit = (celsius * 9/5) + 32
+    return fahrenheit
+
+
+
+
+try:
+ user_input=(input("Enter a temperature and its unit (e.g., 25 C or 77 F )"))
+ temp=user_input.split(" ")
+ unit = temp[1]
+ 
+ value = float(temp [0]) 
+ if unit == 'C':
+    print(f"Temperature in Fahrenheit: {celsius_to_fahrenheit(value)} F")
+ elif unit == 'F':
+    print(f"Temperature in Celsius: {fahrenheit_to_celsius(value)} C")
+ else:
+   print("Invalid unit. Please use 'C' for Celsius or 'F' for Fahrenheit.")
+
+
+except ValueError:
+ print("Invalid value. Please make sure to enter a valid number.")
+
+except TypeError:
+   print("Invalid unit. Please use 'C' for Celsius or 'F' for Fahrenheit.")
+   
+
+
+ 
+
+
+
+
+
