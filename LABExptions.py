e2f9bd203a37686cafcf232202fbebb12aee94f4
suha <suha.alrajhi01@gmail.com>
2023-07-31 20:42:56 +0300

    LabExptions suha alrajhi

---
+++ LABExptions.py
@@ -0,0 +1,12 @@
+#labExptions
+
+def additoin(x, y):
+    try:
+        x = 10
+        y = 20
+        print("Addition:", x + b)
+        print("the operation is successful")
+    except NameError:
+        print("Variable b is not defined.")
+
+additoin(10, 20)
