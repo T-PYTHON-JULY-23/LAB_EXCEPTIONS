da91dd7e9a6179046091b1c7339c368e6ea636b8
Atheer Abdullah <Atheer98.abdullah19@gmail.com>
2023-07-31 17:27:25 +0300

    lab

---
+++ lab.py
@@ -0,0 +1,11 @@
+def additoin(x, y):
+    x = 10
+    y = 20
+    try:
+        print("Addition:", x + b)
+        print("the operation is successful")
+    except NameError:
+        print("variable not defined")
+
+
+additoin(10, 20)
