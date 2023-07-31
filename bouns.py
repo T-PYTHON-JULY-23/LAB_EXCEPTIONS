eaf963593d5885e77d3398af679cc23702832547
renad1 <renadxx17@gmail.com>
2023-07-31 17:28:27 +0300

    Bonus

---
+++ bouns.py
@@ -0,0 +1,29 @@
+def celsius_to_fahrenheit(celsius):
+    fahrenheit = (celsius * 9/5) + 32
+    return fahrenheit
+def fahrenheit_to_celsius(fahrenheit):
+    celsius = (fahrenheit - 32) * 5/9
+    return celsius
+t = True
+while t == True:
+    userInput = input('Enter a temperature and its unit (e.g., "25 C" or "77 F"): ')
+    try: 
+        m=len(userInput) - 2
+        temp = int(userInput[:m])
+        unit= userInput[m+1:]
+        if (unit.upper() == 'C'):
+            tempF= celsius_to_fahrenheit(temp)
+            print(f'Temperature in Fahrenheit: {round(tempF,2)} {unit.upper}')
+            t = False
+        elif(unit.upper() =='F'):
+            tempC= fahrenheit_to_celsius(temp)
+            print(f'Temperature in Celsius: {round(tempC,2)} C')
+            t= False
+        else:
+            print('Invalid unit. Please use (C) for Celsius or (F) for Fahrenheit, try again')
+    except ValueError as e :
+        print(e)
+        print('invalid temperature value try again')
+    except TypeError as e:
+            print(e)
+            print('invalid temperature unit try again')
