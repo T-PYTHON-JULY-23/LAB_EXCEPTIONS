bf0de7d81a410a4bf1bfc29e250f2e16484a8028
Lenah11 <lenhh11@gmail.com>
2023-08-01 08:45:31 +0300

    Bouns

---
+++ lab_exceptions.py
@@ -0,0 +1,17 @@
+
+def additoin(x, y):
+    x = 10
+    y = 20
+
+    print("Addition:", x + b)
+ 
+
+
+try:
+    additoin(10, 20)
+except NameError:
+    print("name b is not defind" )
+else:
+   print("Operation/s inside Try is successful")
+
+
