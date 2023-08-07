25cc8664a356502a39ba2ee79cab0d601f463363
sarahnaghi <sara.naghi@gmail.com>
2023-08-07 10:02:17 +0300

    lab exception edit

--- LAB_EXCEPTIONS.py
+++ LAB_EXCEPTIONS.py
@@ -1,14 +1,21 @@
 
-
 def additoin(x, y):
     x = 10
     y = 20
-try:
     print("Addition:", x + b) 
     print("the operation is successful")
 
+
+try:
+ additoin(10, 20)
 except NameError:
         print("not defined variable! make sure you define your variables")
 
-additoin(10, 20)
+except:
+        print("Somthing went wrong!")
+        
+
+ 
+
+
 
--- bonus.py
+++ bonus.py
@@ -9,29 +9,29 @@ def celsius_to_fahrenheit(celsius):
 
 
 
+def main():
+   while True:
+       try:
+         user_input=(input("Enter a temperature and its unit (e.g., 25 \"C\" or 77 \"F\" )"))
+         if user_input== "exit":
+           break
+         temp=user_input.split(" ")
+         unit = temp[1]
+         value = float(temp [0]) 
 
-try:
- user_input=(input("Enter a temperature and its unit (e.g., 25 C or 77 F )"))
- temp=user_input.split(" ")
- unit = temp[1]
+         if unit.upper() == 'C':
+          print(f"Temperature in Fahrenheit: {celsius_to_fahrenheit(value)} F")
+         elif unit.upper() == 'F':
+          print(f"Temperature in Celsius: {fahrenheit_to_celsius(value)} C")
+         else:
+          raise TypeError ("Invalid unit. Please use 'C' for Celsius or 'F' for Fahrenheit.")
+
+       except ValueError:
+        print("Invalid value. Please make sure to enter a valid number.")
+       except TypeError as e:
+        print(e)
  
- value = float(temp [0]) 
- if unit == 'C':
-    print(f"Temperature in Fahrenheit: {celsius_to_fahrenheit(value)} F")
- elif unit == 'F':
-    print(f"Temperature in Celsius: {fahrenheit_to_celsius(value)} C")
- else:
-  raise TypeError ("Invalid unit. Please use 'C' for Celsius or 'F' for Fahrenheit.")
-
-
-except ValueError:
- print("Invalid value. Please make sure to enter a valid number.")
-
-   
-
-
- 
-
+main()
 
 
 
